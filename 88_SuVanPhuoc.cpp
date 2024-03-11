bb17873faa932069031f75ecc30784ec429b9e98
unknown <phuoclanh78@gmail.com>
2024-03-11 10:47:30 +0700

    2151053048_SuVanPhuoc_File thay doi tai nhanh main

---
+++ 88_SuVanPhuoc.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Hello World" << endl;
+	cout << "Xin chao minh la Phuoc" << endl;
+	cout << "I'm a robot, aren;t I?"<<endl;
+	system("pause");
+	return 0;
+}
