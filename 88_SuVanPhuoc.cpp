97cae93adcc6374b11323df14190c31e40cb7643
PhuocSu <111679170+PhuocSu@users.noreply.github.com>
2024-03-11 11:37:35 +0700

    Update 88_SuVanPhuoc.cpp

--- 88_SuVanPhuoc.cpp
+++ 88_SuVanPhuoc.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 int main() {
-	cout << "Hello My Teacher" << endl;
-	cout << "Xin chao minh la Phuoc" << endl;
+	cout << "Hello Mytttt Teacher" << endl;
+	cout << "Xin chao minh la tttttttPhuoc" << endl;
 	cout << "MSSV: 2151053048" << endl;
 	system("pause");
 	return 0;
